123bea670588e46f24af1047c55af5f7e9129569
Bhupendra1449 <sameer.bhupi@gmail.com>
2025-09-15 02:26:54 +0530

    utils files create

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 import app from "./app.js";
-import connectionWithDatabase from "./db/index.js";
+import connectDB from "./database/index.js";
+import dotenv from 'dotenv'
+
+dotenv.config({ path:'./env' })
 
 
-connectionWithDatabase()
+
+connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 5000 ,()=>{
-        console.log(`Server Start at port http://localhost:${process.env.PORT}`); 
-    }
-    )
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : https://localhost:${process.env.PORT}`);
+        
+    })
+    
+}).catch((error)=>{
+    console.log("Server running Failed")
 })
+    
